d9b50e7a231066c38fdc1dabe90abe453cf4953d
ryan <ryanjohnparry@gmail.com>
2021-05-13 16:49:57 +0200

    mute when ads playing

--- skipads.js
+++ skipads.js
@@ -1,35 +1,62 @@
 //Youtube one-click ads skipper.
 (function () {
-  //look for a button with the text "skip ads"
+  showMessage();
+  let mutedBecauseAd = false;
   setInterval(() => {
-    let b = [...document.querySelectorAll('button')]
-      .filter(x => x.textContent.toLowerCase().indexOf('skip ad')!=-1)[0];
-    let b2 = document.querySelector(".ytp-ad-overlay-close-button");
-    if (b) b.click();
-    // @ts-ignore
-    if (b2) b2.click();
+    clickSkipButton();
+    closeBannerAd();
+    muteIfAdRunning();
   }, 500);
 
-  //create banner for top of screen
-  let e = document.createElement("div");
-  e.innerText = 'From now on Ads will be skipped on this page.'
-    + 'If a new webpage is loaded, you will need to click again for the new webpage.';
-  let sheet = document.createElement('style');
-  sheet.innerHTML = `#msg_b_._fade {
+  function clickSkipButton() {
+    let b = [...document.querySelectorAll('button')]
+      .filter(x => ~x.textContent.search(/[sS]kip [aA]d/))[0];
+    if (b) b.click();
+  }
+  function closeBannerAd() {
+    let b2 = document.querySelector(".ytp-ad-overlay-close-button");
+    // @ts-ignore
+    if (b2) b2.click();
+  }
+  function muteIfAdRunning() {
+    //also unmutes if Ad not running
+    let adElem = document.querySelector(".ytp-ad-text");
+    let muteBtn = document.querySelector(".ytp-mute-button");
+    let adRunning = !!adElem;
+    if (adRunning) {
+      isMuted = !!~muteBtn.ariaLabel.toLowerCase().search("unmute");
+      if (!isMuted) {
+        muteBtn.click();
+        mutedBecaseAd = true;
+      }
+    } else {
+      if (mutedBecauseAd) {
+        muteBtn.click();
+        mutedBecauseAd = false;
+      }
+    }
+  }
+  function showMessage() {
+    //create banner for top of screen
+    let e = document.createElement("div");
+    e.innerText = 'Ads will be skipped on this page.';
+    let sheet = document.createElement('style');
+    sheet.innerHTML = `#msg_b_._fade {
   opacity:0;
-}
-#msg_b_{
-  background-color:green;
-  color:black;
-  position:fixed;
-  font-size:14px;
-  top:2px;
-  z-index:99999;
-  opacity:1;
-  transition:opacity 5s;
-   }
-`;
-  e.id = "msg_b_";
-  document.body.append(sheet, e);
-  setTimeout(() => e.classList.add('_fade'), 2000);
+  }
+  #msg_b_{
+    background-color:LemonChiffon;
+    color:LightCoral;
+    position:fixed;
+    font-size:24;
+    top:2px;
+    z-index:99999;
+    opacity:1;
+    transition:opacity 5s;
+    }
+  `;
+    e.id = "msg_b_";
+    document.body.append(sheet, e);
+    setTimeout(() => e.classList.add('_fade'), 2000);
+  }
 })();
