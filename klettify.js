3f2a1edf9a47075c83ccc65fea10dcbcae1d6fe1
ryan <ryanjohnparry@gmail.com>
2021-05-16 13:42:07 +0200

    add input and output

--- klettify.js
+++ klettify.js
@@ -1,12 +1,24 @@
 //klettify
+const fs = require('fs')
+const GREEN = "\x1b[32m";
+const RED = "\x1b[31m";
+const YELLOW = "\x1b[33m";
+const WHITE = "\x1b[37m";
+const RESET = "\x1b[0m";
+const BLUE = "\x1b[34m"
+const MAGENTA = "\x1b[35m"
+
 let myArgs = process.argv.slice(2);
-let switches="";
-if (myArgs.length>2){
+let switches = "";
+if (myArgs.length > 2) {
   switches = myArgs.shift();
-} 
-let inputArg=myArgs[0];
-let outputArg=myArgs[1];
+}
+let inputArg = myArgs[0];
+let outputArg = myArgs[1];
+
 let inputText = getInput();
+console.log(YELLOW, "Running klettify...");
+console.log(MAGENTA, "INPUT : \n" + inputText);
 
 let out = stripComments(inputText);
 out = makeOneliner(out);
@@ -14,6 +26,7 @@ out = minimiseSpaces(out);
 out = makeIIFE(out);
 out = addNecessaryPrefix(out);
 
+console.log(BLUE, "OUTPUT : \n " + out);
 sendOutput(out);
 
 function minimiseSpaces(text) {
@@ -38,20 +51,22 @@ function makeIIFE(text) {
 }
 
 function getInput() {
-  return `
-var thisIsATest=3;
-//to see if javascript can be turned into bookmarklets
-console.log(thisIsATest**3);
-
-function something(x){
-  return x**2 + 2*x + 1
-};
-
-console.log(something(12));
-
-`;
+  try {
+    const data = fs.readFileSync(inputArg, 'utf8')
+    return data;
+  } catch (err) {
+    console.error(RED, "Could not read contents of input file: " + inputArg + "\n" + err);
+  }
 }
 
 function sendOutput(out) {
-  console.log(out);
+  fs.writeFile(outputArg, out, err => {
+    if (err) {
+      console.error("Could not write to output file: " + outputArg + "\n" + err);
+      console.log(RESET, "");
+      return false;
+    }
+    console.log(GREEN, "\nBookmarklet creation was successful");
+    console.log(RESET);
+  })
 }
