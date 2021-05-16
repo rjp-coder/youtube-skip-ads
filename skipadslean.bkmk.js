c07206e8050f8d0deff13735808daf61869b59d3
ryan <ryanjohnparry@gmail.com>
2021-05-16 22:55:24 +0200

    add lean skipads

---
+++ skipadslean.bkmk.js
@@ -0,0 +1,1 @@
+javascript:(function(){(() => { setInterval(() => { ö = [...document.querySelectorAll('button')] .filter(x => ~x.textContent.search(/[sS]kip [aA]d/))[0]; if (ö) ö.click(); }, 500); })(); }());
